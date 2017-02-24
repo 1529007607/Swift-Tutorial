be837fe67486e1fa697495393d6fdfc704298335
1529007607 <1529007607@qq.com>
2017-02-24 20:05:39 +0800

    Url encode & decode
    20% <-> whitespace

---
+++ Url encode & decode.playground/Contents.swift
@@ -0,0 +1,9 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var s = "aa bb -[:/?&=;+!@#$()',*]";
+
+let sEncode = s.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)
+
+let sDecode = sEncode?.removingPercentEncoding
